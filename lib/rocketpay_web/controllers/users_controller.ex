9bc19cbf899732d9b2f353d27317b537af9ec63e
Thales Testoni <thalestestoni@gmail.com>
2021-02-27 18:45:47 -0300

    Fix accounts and users controller

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -1,15 +1,15 @@
 defmodule RocketpayWeb.UsersController do
   use RocketpayWeb, :controller
 
-  alias Rocketpay.Account
+  alias Rocketpay.User
 
   action_fallback RocketpayWeb.FallbackController
 
-  def deposit(conn, params) do
-    with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+  def create(conn, params) do
+    with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
       conn
-      |> put_status(:ok)
-      |> render("update.json", account: account)
+      |> put_status(:created)
+      |> render("create.json", user: user)
     end
   end
 end
